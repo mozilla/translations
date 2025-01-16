9e46ca5f203ebb1b65358b296eda11dcd1f3df38
evgeny pavlov <epavlov@mozilla.com>
2025-01-15 16:05:20 -0800

    Clean temporary files

--- pipeline/clean/clean-mono.sh
+++ pipeline/clean/clean-mono.sh
@@ -100,7 +100,7 @@ echo "Lines after fluency filtering: $(zstdmt -dc "${output_prefix}.${lang}.zst"
 ######################################################################
 echo "### Remove data from intermediate steps"
 rm -rf "${output_prefix}".*.nrm.zst "${output_prefix}".*.langid.zst \
-  "${output_prefix}".*.monofix.zst ${dir}/monocleaner
+  "${output_prefix}".*.monofix.zst "${output_prefix}".*.rule-based.zst ${dir}/monocleaner
 
 echo "### Rule-based cleaning log written to: ${output_prefix}.${lang}.clean.debug.txt"
 echo "### Clean data is written to: ${output_prefix}.${lang}.zst"
