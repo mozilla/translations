ecbd7d93b21ff5d1adb62a7d8d85e67d4cdc322a
evgeny pavlov <epavlov@mozilla.com>
2025-02-12 16:03:20 -0800

    Fix handling parallel corpus

--- pipeline/data/cjk.py
+++ pipeline/data/cjk.py
@@ -1,6 +1,7 @@
 """
 Chinese, Japanese, Korean (CJK) specific data importing code
 """
+import shutil
 from enum import Flag
 from pathlib import Path
 from typing import Optional
@@ -10,7 +11,9 @@ import opencc
 
 from pipeline.common.datasets import Statistics
 from pipeline.common.downloads import read_lines, write_lines
+from pipeline.common.logging import get_logger
 
+logger = get_logger(__file__)
 
 CJK_LANGS = ["zh", "ja", "ko"]
 
@@ -41,7 +44,7 @@ class DatasetStatistics(Statistics):
         super().__init__(dataset_path)
         self.script = script
         self.script_conversion = ConversionStep(
-            f"How many sentences in the dataset were converted to {script.name}",
+            f"How many sentences in the dataset were converted to {script.name} or filtered",
         )
 
 
@@ -85,6 +88,35 @@ class ChineseConverter:
 
         return stats
 
+    def filter_parallel_corpus(
+        self,
+        zh_path: Path,
+        other_path: Path,
+        zh_output_path: Path,
+        other_output_path: Path,
+        variant: ChineseType,
+    ):
+        """
+        Filter everything except the specified variant of Chinese in a parallel corpus
+        """
+        stats = DatasetStatistics(zh_output_path, variant)
+        with (
+            write_lines(zh_output_path) as zh_out_file,
+            write_lines(other_output_path) as other_out_file,
+            read_lines(zh_path) as zh_lines,
+            read_lines(other_path) as other_lines,
+        ):
+            for zh_line, other_line in zip(zh_lines, other_lines):
+                stats.script_conversion.visited += 1
+                ch_type = self._detect(zh_line)
+                if ch_type == variant:
+                    zh_out_file.write(zh_line)
+                    other_out_file.write(other_line)
+                else:
+                    stats.script_conversion.filtered += 1
+
+        return stats
+
     @staticmethod
     def _detect(text) -> ChineseType:
         res = hanzidentifier.identify(text)
@@ -102,3 +134,50 @@ class ChineseConverter:
         elif to == ChineseType.traditional:
             return self.s2t.convert(text)
         raise ValueError(f"Unsupported type: {to}")
+
+
+def handle_chinese_mono(file_destination: Path, is_src: bool, variant: ChineseType):
+    converted_path = file_destination.with_suffix(".converted.zst")
+    chinese_converter = ChineseConverter()
+    if is_src:
+        logger.info(f"Converting the output file to {variant}")
+        stats = chinese_converter.convert_file(file_destination, converted_path, variant)
+    else:
+        logger.info(f"Filtering out everything except {variant} in the output file")
+        stats = chinese_converter.filter_file(file_destination, converted_path, variant)
+    shutil.move(converted_path, file_destination)
+    print(
+        f"Converted {stats.script_conversion.converted}, Filtered: {stats.script_conversion.filtered} Visited: {stats.script_conversion.visited}"
+    )
+    stats.save_json()
+
+
+def handle_chinese_parallel(output_prefix: str, src: str, trg: str, variant: ChineseType):
+    if "zh" not in (src, trg):
+        raise ValueError("Run only for Chinese")
+
+    chinese_converter = ChineseConverter()
+    is_src = src == "zh"
+    if is_src:
+        logger.info(f"Converting the output file to {variant}")
+        stats = chinese_converter.convert_file(
+            input_path=Path(f"{output_prefix}.{src}.zst"),
+            output_path=Path(f"{output_prefix}.converted.{src}.zst"),
+            to=variant,
+        )
+        shutil.move(f"{output_prefix}.converted.{src}.zst", f"{output_prefix}.{src}.zst")
+    else:
+        logger.info(f"Filtering out everything except {variant} from a parallel corpus")
+        stats = chinese_converter.filter_parallel_corpus(
+            zh_path=Path(f"{output_prefix}.{trg}.zst"),
+            other_path=Path(f"{output_prefix}.{src}.zst"),
+            zh_output_path=Path(f"{output_prefix}.filtered.{trg}.zst"),
+            other_output_path=Path(f"{output_prefix}.filtered.{src}.zst"),
+            variant=variant,
+        )
+        shutil.move(f"{output_prefix}.filtered.{trg}.zst", f"{output_prefix}.{trg}.zst")
+        shutil.move(f"{output_prefix}.filtered.{src}.zst", f"{output_prefix}.{src}.zst")
+    print(
+        f"Converted {stats.script_conversion.converted}, Filtered: {stats.script_conversion.filtered} Visited: {stats.script_conversion.visited}"
+    )
+    stats.save_json()
--- pipeline/data/dataset_importer.py
+++ pipeline/data/dataset_importer.py
@@ -15,10 +15,8 @@ import argparse
 import os
 import random
 import re
-import shutil
 import subprocess
 import sys
-from pathlib import Path
 from typing import Dict, Iterable, List
 
 from opustrainer.modifiers.noise import NoiseModifier
@@ -28,7 +26,7 @@ from opustrainer.modifiers.typos import TypoModifier
 from opustrainer.types import Modifier
 
 from pipeline.common.downloads import compress_file, decompress_file
-from pipeline.data.cjk import ChineseConverter, ChineseType
+from pipeline.data.cjk import handle_chinese_parallel, ChineseType
 
 random.seed(1111)
 
@@ -244,8 +242,12 @@ def run_import(
             [os.path.join(current_dir, "download-corpus.sh"), no_aug_id, output_prefix],
             env={"SRC": src, "TRG": trg},
         )
-
-        handle_chinese(output_prefix, src, trg)
+        # TODO: convert everything to Chinese simplified for now when Chinese is the source language
+        # TODO: https://github.com/mozilla/firefox-translations-training/issues/896
+        if "zh" in (src, trg):
+            handle_chinese_parallel(
+                output_prefix, src=src, trg=trg, variant=ChineseType.simplified
+            )
 
         if aug_modifer:
             print("Running augmentation")
@@ -255,39 +257,6 @@ def run_import(
         raise ValueError("Downloading mono data is not supported yet")
     else:
         raise ValueError(f"Invalid dataset type: {type}. Allowed values: mono, corpus")
-
-
-def handle_chinese(output_prefix, src, trg):
-    for lang in (src, trg):
-        if lang == "zh":
-            # TODO: convert everything to Chinese simplified for now when Chinese is the source language
-            # TODO: https://github.com/mozilla/firefox-translations-training/issues/896
-            if lang == src:
-                print("Converting the output file to Chinese Simplified")
-                chinese_converter = ChineseConverter()
-                stats = chinese_converter.convert_file(
-                    Path(f"{output_prefix}.{lang}.zst"),
-                    Path(f"{output_prefix}.converted.{lang}.zst"),
-                    ChineseType.simplified,
-                )
-                shutil.move(f"{output_prefix}.converted.{lang}.zst", f"{output_prefix}.{lang}.zst")
-                print(
-                    f"Converted {stats.script_conversion.converted} lines from {stats.script_conversion.visited} to Chinese Simplified"
-                )
-                stats.save_json()
-            else:
-                print("Filtering out Chinese Traditional in the output file")
-                chinese_converter = ChineseConverter()
-                stats = chinese_converter.filter_file(
-                    Path(f"{output_prefix}.{lang}.zst"),
-                    Path(f"{output_prefix}.filtered.{lang}.zst"),
-                    ChineseType.simplified,
-                )
-                shutil.move(f"{output_prefix}.filtered.{lang}.zst", f"{output_prefix}.{lang}.zst")
-                print(
-                    f"Filtered {stats.script_conversion.filtered} lines from {stats.script_conversion.visited}"
-                )
-                stats.save_json()
 
 
 def main() -> None:
