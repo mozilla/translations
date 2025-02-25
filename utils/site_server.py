74362248a831b48ae648375989d0f3501645ed8b
Greg Tatum <tatum.creative@gmail.com>
2025-02-25 13:39:13 -0600

    Add a site_server script so that local development does not cache assets

---
+++ utils/site_server.py
@@ -0,0 +1,38 @@
+import http.server
+import socketserver
+
+PORT = 8080
+DIRECTORY = "site"
+
+"""
+Configures the Python http server to serve the sites folder without caching.
+"""
+
+
+class NoCacheHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=DIRECTORY, **kwargs)
+
+    def end_headers(self):
+        self.send_header("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0")
+        self.send_header("Pragma", "no-cache")
+        self.send_header("Expires", "0")
+        super().end_headers()
+
+
+def run_server():
+    with socketserver.TCPServer(("", PORT), NoCacheHTTPRequestHandler) as server:
+        print("To rebuild the docs run:")
+        print(" - task build-docs")
+        print("Or live-reload the docs with: ")
+        print(" - task serve-docs\n")
+        print(f"Serving '{DIRECTORY}' directory at http://localhost:{PORT}\n")
+
+        try:
+            server.serve_forever()
+        except KeyboardInterrupt:
+            server.socket.close()
+
+
+if __name__ == "__main__":
+    run_server()
