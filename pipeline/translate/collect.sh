64e0751b0d685a43adaf46bb60e4c5b55a85f8ef
Greg Tatum <tatum.creative@gmail.com>
2025-04-09 09:59:58 -0500

    Rename translate-mono-src to translate-distillation-mono-src

--- pipeline/translate/collect.sh
+++ pipeline/translate/collect.sh
@@ -8,7 +8,7 @@
 # Example tasks running on chunked data (before this script):
 #   extract-best-en-ca-1/10
 #   translate-distillation-parallel-src-en-ca-1/10
-#   translate-mono-src-en-ca-1/10
+#   translate-distillation-mono-src-en-ca-1/10
 #   translate-mono-trg-en-ca-1/10
 #
 # Kinds:
