5b05b7315fd5471c3bde263ea113362d37631d24
Greg Tatum <tatum.creative@gmail.com>
2025-04-09 09:41:57 -0500

    split-corpus to distillation-parallel-src-chunk

--- tests/test_split_parallel.py
+++ tests/test_split_parallel.py
@@ -38,7 +38,7 @@ def test_split_corpus():
     data_dir.create_zst(
         "fetches/corpus.ru.zst", "\n".join([f"ru-{i}" for i in range(corpus_line_count)]) + "\n"
     )
-    data_dir.run_task("split-corpus-en-ru")
+    data_dir.run_task("distillation-parallel-src-chunk-en-ru")
     data_dir.print_tree()
 
     for i in range(10):
