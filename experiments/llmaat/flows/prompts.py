ed0b0b312eb361c268674520c1821c5444ee25b9
evgeny pavlov <epavlov@mozilla.com>
2025-05-16 11:35:11 -0700

    Add few shot prompt

--- experiments/llmaat/flows/prompts.py
+++ experiments/llmaat/flows/prompts.py
@@ -36,4 +36,55 @@ def wmt24pp(text, from_lang_code, to_lang_code):
     return system_prompt, user_prompt
 
 
-prompts = {"chat-gpt": chat_gpt, "basic": basic, "wmt24pp": wmt24pp}
+examples = {
+    "en_US-ru_RU": [
+        ("- Online: http://wrathofman.movie", "- Онлайн: http://wrathofman.movie"),
+        ("Growth Opportunity Analysis - FWD", "Анализ возможностей роста - FWD"),
+        (
+            "Oh wow - those colors are gorgeous! Love the butterfly!ReplyDelete",
+            "О, вау — какие потрясающие цвета! Обожаю бабочку!ОтветитьУдалить",
+        ),
+        (
+            "When you need a break, order a festive cocktail and pizza from the menu, and watch your friends defeat their opponents.",
+            "Когда вам понадобится перерыв, закажите праздничный коктейль и пиццу из меню и наблюдайте, как ваши друзья побеждают своих соперников.",
+        ),
+        (
+            "In other trading, benchmark U.S. crude rose 34 cents to $52.61 a barrel in electronic trading on the New York Mercantile Exchange. It lost 86 cents to $52.27 per barrel on Friday. Brent crude, the international standard edged up 30 cents to $55.71 a barrel.",
+            "В других торгах эталонная американская нефть подорожала на 34 цента до $52,61 за баррель в ходе электронных торгов на Нью-Йоркской товарной бирже. В пятницу она подешевела на 86 центов до $52,27 за баррель. Нефть Brent, международный стандарт, прибавила 30 центов до $55,71 за баррель.",
+        ),
+    ]
+}
+
+
+def no_omit_few_shot(text, from_lang_code, to_lang_code):
+    from_lang = LANGS[from_lang_code]["lang"]
+    to_lang = LANGS[to_lang_code]["lang"]
+    ex_str = "\n\n".join(
+        [
+            f"{from_lang}: {src}\n{to_lang}: {trg}"
+            for src, trg in examples[f"{from_lang_code}-{to_lang_code}"]
+        ]
+    )
+    system_prompt = (
+        f"""You are a professional translator. 
+Translate the user's text from {from_lang} to {to_lang}. 
+Respond with a translation only. 
+Do not omit anything in the translation.
+Preserve the meaning as close as possible to the original text, even if doesn't make sense. 
+The generated translations will be used to train a neural machine translation model.
+
+Examples:
+
+"""
+        + ex_str
+    )
+    user_prompt = f"{from_lang}: {text}\n{to_lang}: "
+    return system_prompt, user_prompt
+
+
+prompts = {
+    "chat-gpt": chat_gpt,
+    "basic": basic,
+    "wmt24pp": wmt24pp,
+    "noomit_fewshot": no_omit_few_shot,
+}
