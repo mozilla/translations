30b49701909f4bf8f8588b04ee6bed0bb879c00d
Greg Tatum <tatum.creative@gmail.com>
2025-05-20 09:47:53 -0500

    Re-use the local development container when possible

--- utils/tasks/docker-run.py
+++ utils/tasks/docker-run.py
@@ -6,6 +6,9 @@ import os
 import platform
 import sys
 
+CONTAINER_NAME = "translations-local-dev"
+IMAGE_NAME = "translations-local"
+
 
 def get_args():
     parser = argparse.ArgumentParser(description="Run the local docker image.")
@@ -23,50 +26,94 @@ def get_args():
         help="Run the Docker container as the current user's UID and GID.",
     )
 
+    parser.add_argument(
+        "--clean",
+        action="store_true",
+        help="Remove any existing container before starting a new one.",
+    )
+
+    parser.add_argument(
+        "--name",
+        type=str,
+        default=CONTAINER_NAME,
+        help="Provide a custom container name",
+    )
+
     args, other_args = parser.parse_known_args()
     args.other_args = other_args
 
     return args
 
 
+def container_exists(container_name: str):
+    command = [
+        "docker", "ps",
+        "--all",
+        "--filter", f"name={container_name}",
+        "--format", "{{.Names}}"
+    ] # fmt: skip
+    result = subprocess.run(
+        command,
+        stdout=subprocess.PIPE,
+        text=True,
+        check=False,
+    )
+    return container_name in result.stdout.splitlines()
+
+
 def main():
     args = get_args()
 
-    docker_command = [
-        "docker",
-        "run",
-        "--interactive",
-        "--tty",
-        "--rm",
-        "--volume", f"{os.getcwd()}:/builds/worker/checkouts",
-        "--workdir", "/builds/worker/checkouts",
-        "--expose", "8000", # Expose the mkdocs connection
-    ]  # fmt: skip
+    container_name: str = args.name
 
-    # Export the host operating system as an environment variable within the container.
-    host_os = platform.system()
-    docker_command.extend(["--env", f"HOST_OS={host_os}"])
+    if args.clean and container_exists(container_name):
+        print(f'[docker-run] Removing the "{container_name}" container')
+        subprocess.run(["docker", "container", "rm", "--force", container_name], check=True)
 
-    # Add additional volumes if provided
-    if args.volume:
-        for volume in args.volume:
-            docker_command.extend(["--volume", volume])
+    if container_exists(container_name):
+        print(f"[docker-run] Attaching to the existing container {container_name}")
+        result = subprocess.run(
+            ["docker", "container", "start", "--attach", "--interactive", container_name],
+            check=False,
+        )
+    else:
+        # Create and run the container.
+        docker_command = [
+            "docker", "container", "run",
+            "--interactive",
+            "--tty",
+            "--name", container_name,
+            "--volume", f"{os.getcwd()}:/builds/worker/checkouts",
+            "--workdir", "/builds/worker/checkouts",
+            "--publish", "8000:8000",
+            "--publish", "8080:8080"
+        ]  # fmt: skip
 
-    # Run Docker with the current user's UID and GID if --run-as-user is specified
-    if args.run_as_user:
-        uid = os.getuid()
-        gid = os.getgid()
-        docker_command.extend(["--user", f"{uid}:{gid}"])
+        # Export the host operating system as an environment variable within the container.
+        host_os = platform.system()
+        docker_command.extend(["--env", f"HOST_OS={host_os}"])
 
-    # Specify the Docker image
-    docker_command.append("translations-local")
+        # Add additional volumes if provided
+        if args.volume:
+            for volume in args.volume:
+                docker_command.extend(["--volume", volume])
 
-    # Append any additional args
-    if args.other_args:
-        docker_command.extend(args.other_args)
+        # Run Docker with the current user's UID and GID if --run-as-user is specified
+        if args.run_as_user:
+            uid = os.getuid()
+            gid = os.getgid()
+            docker_command.extend(["--user", f"{uid}:{gid}"])
 
-    print("Executing command:", " ".join(docker_command))
-    result = subprocess.run(docker_command, check=False)
+        # Specify the Docker image
+        docker_command.append("translations-local")
+
+        # Append any additional args
+        if args.other_args:
+            docker_command.extend(args.other_args)
+
+        print("[docker-run] Running:", " ".join(docker_command))
+        result = subprocess.run(docker_command, check=False)
+
     sys.exit(result.returncode)
 
 
