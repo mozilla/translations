4a6a44c03e545503b83fe30a4d1f18accfcbc42e
Evgeny Pavlov <epavlov@mozilla.com>
2025-08-19 15:43:21 -0700

    Improvements for LLM dataset generation (#1206)
    * Add support for more models
    
    * Enable and fix QE reranking
    
    * Add sampling config
    
    * Increase disk
    
    * Vllm update and other fixes
    
    * Add support for gpt-oss
    
    * Fix output parsing
    
    * Increase disk and add comments

--- experiments/llmaat/flows/llm_runner.py
+++ experiments/llmaat/flows/llm_runner.py
@@ -45,18 +45,18 @@ class GenericModel(Model):
             {"role": "user", "content": user_prompt},
         ]
 
+    def get_prompt(self, text, from_lang, to_lang, prompt_type):
+        chat_style_prompt = self.get_chat_prompt(text, from_lang, to_lang, prompt_type)
+        chat_prompt = self.tokenizer.apply_chat_template(
+            chat_style_prompt, tokenize=False, add_generation_prompt=True
+        )
+        return chat_prompt
+
     def translate_batch(self, texts, from_lang, to_lang, params):
         import toolz
 
-        def get_prompt(text, from_lang, to_lang):
-            prompt_type = params["prompt"]
-            chat_style_prompt = self.get_chat_prompt(text, from_lang, to_lang, prompt_type)
-            chat_prompt = self.tokenizer.apply_chat_template(
-                chat_style_prompt, tokenize=False, add_generation_prompt=True
-            )
-            return chat_prompt
-
-        prompts = [get_prompt(text, from_lang, to_lang) for text in texts]
+        prompt_type = params["prompt"]
+        prompts = [self.get_prompt(text, from_lang, to_lang, prompt_type) for text in texts]
 
         max_input_tokens = max(len(tokens) for tokens in self.tokenizer(texts).input_ids)
         max_tok_alpha = params["max_tok_alpha"]
@@ -102,6 +102,45 @@ class GenericModel(Model):
         return processed_outputs
 
 
+class Qwen3(GenericModel):
+    def __init__(self, size, active, fp8, instruct):
+        super().__init__()
+        self.size = size
+        self.fp8 = fp8
+        self.active = active
+        self.instruct = instruct
+
+    def get_prompt(self, text, from_lang, to_lang, prompt_type):
+        chat_style_prompt = self.get_chat_prompt(text, from_lang, to_lang, prompt_type)
+        # add enable_thinking=False
+        chat_prompt = self.tokenizer.apply_chat_template(
+            chat_style_prompt, tokenize=False, add_generation_prompt=True, enable_thinking=False
+        )
+        return chat_prompt
+
+    def get_repo(self, target_lang):
+        suffix = ""
+        # add support for MoE models
+        if self.active:
+            suffix += f"-A{self.active}B"
+        # add support for Instruct models
+        if self.instruct:
+            suffix += "-Instruct-2507"
+        # add support for float8 quantization
+        if self.fp8:
+            suffix += "-FP8"
+        return f"Qwen/Qwen3-{self.size}B{suffix}"
+
+
+class GptOss(GenericModel):
+    def __init__(self, size):
+        super().__init__()
+        self.size = size
+
+    def get_repo(self, target_lang):
+        return f"openai/gpt-oss-{self.size}b"
+
+
 class Gemma3(GenericModel):
     def __init__(self, size):
         super().__init__()
@@ -128,6 +167,21 @@ class Gemma3(GenericModel):
             },
             {"role": "user", "content": [{"type": "text", "text": user_prompt}]},
         ]
+
+
+class Gemma3n(Gemma3):
+    def get_repo(self, target_lang):
+        return f"google/gemma-3n-E{self.size}B-it"
+
+
+class Gemma3FP8(Gemma3):
+    def get_repo(self, target_lang):
+        return f"RedHatAI/gemma-3-{self.size}b-it-FP8-dynamic"
+
+
+class Gemma3w4a16(Gemma3):
+    def get_repo(self, target_lang):
+        return f"RedHatAI/gemma-3-{self.size}b-it-quantized.w4a16"
 
 
 class Llama3(GenericModel):
@@ -221,14 +275,19 @@ class VllmModel(GenericModel):
         from transformers import AutoTokenizer
 
         self.tokenizer = AutoTokenizer.from_pretrained(model_path)
-        self.llm = LLM(model=model_path, **params["llm"])
+        self.llm = LLM(model=model_path, trust_remote_code=True, **params["llm"])
 
     def run(self, max_new_tokens, params, prompts):
         from vllm import SamplingParams
 
         outputs = self.llm.generate(
             prompts,
-            SamplingParams(max_tokens=max_new_tokens, **params["decoding"]),
+            SamplingParams(
+                max_tokens=max_new_tokens,
+                # We can stop at newlines. This avoids the model trying to continue generating translations
+                stop=["<|im_end|>", "\n"],
+                **params["decoding"],
+            ),
         )
         return outputs
 
@@ -237,12 +296,125 @@ class VllmModel(GenericModel):
         return [cand.text.strip().split("\n")[0] for output in outputs for cand in output.outputs]
 
 
+class VllmGptOss(GptOss, VllmModel):
+    def create(self, model_path, params):
+        from openai_harmony import HarmonyEncodingName, load_harmony_encoding
+
+        VllmModel.create(self, model_path, params)
+        self.encoding = load_harmony_encoding(HarmonyEncodingName.HARMONY_GPT_OSS)
+
+    def get_prompt(self, text, from_lang, to_lang, prompt_type):
+        from openai_harmony import (
+            Conversation,
+            Message,
+            Role,
+            SystemContent,
+            DeveloperContent,
+            ReasoningEffort,
+        )
+
+        system_prompt, user_prompt = prompts.prompts[prompt_type](
+            text=text, from_lang_code=from_lang, to_lang_code=to_lang
+        )
+
+        # see https://cookbook.openai.com/articles/openai-harmony
+        convo = Conversation.from_messages(
+            [
+                Message.from_role_and_content(
+                    Role.SYSTEM,
+                    # reduce reasoning for faster processing
+                    SystemContent.new().with_reasoning_effort(ReasoningEffort.LOW)
+                    # do not output "commentary" and "analysis" channels
+                    .with_required_channels(["final"]),
+                ),
+                Message.from_role_and_content(
+                    Role.DEVELOPER,
+                    DeveloperContent.new().with_instructions(system_prompt),
+                ),
+                Message.from_role_and_content(Role.USER, user_prompt),
+            ]
+        )
+        return convo
+
+    def run(self, max_new_tokens, params, prompts):
+        from vllm import SamplingParams
+        from openai_harmony import (
+            Role,
+        )
+
+        prefill_ids = [
+            self.encoding.render_conversation_for_completion(prompt, Role.ASSISTANT)
+            for prompt in prompts
+        ]
+        # Harmony stop tokens (pass to sampler so they won't be included in output)
+        stop_token_ids = self.encoding.stop_tokens_for_assistant_actions()
+
+        outputs = self.llm.generate(
+            prompt_token_ids=prefill_ids,
+            sampling_params=SamplingParams(
+                # account for extra tokens when using Harmony message format
+                max_tokens=max_new_tokens + 64,
+                # We can stop at newlines. This avoids the model trying to continue generating translations
+                stop_token_ids=stop_token_ids,
+                **params["decoding"],
+            ),
+        )
+        return outputs
+
+    def parse_outputs(self, outputs):
+        from openai_harmony import (
+            Role,
+        )
+
+        def parse_messages(output):
+            messages = self.encoding.parse_messages_from_completion_tokens(
+                output.token_ids, Role.ASSISTANT
+            )
+            # The messages have this format:
+            # [Message(author=Author(role=<Role.ASSISTANT: 'assistant'>, name=None),
+            #          content=[TextContent(text='translated text')],
+            #          channel='final', recipient=None, content_type=None)]
+            return messages[0].content[0].text
+
+        # Sometimes models output commentary after new lines
+        return [
+            parse_messages(cand).strip().split("\n")[0]
+            for output in outputs
+            for cand in output.outputs
+        ]
+
+
 class VllmLlama3(Llama3, VllmModel):
     def create(self, model_path, params):
         VllmModel.create(self, model_path, params)
 
 
 class VllmGemma3(Gemma3, VllmModel):
+    def create(self, model_path, params):
+        VllmModel.create(self, model_path, params)
+
+
+class VllmGemma3n(Gemma3n, VllmModel):
+    def create(self, model_path, params):
+        VllmModel.create(self, model_path, params)
+
+
+class VllmGemma3FP8(Gemma3FP8, VllmModel):
+    def create(self, model_path, params):
+        VllmModel.create(self, model_path, params)
+
+
+class VllmQwen3FP8(Gemma3FP8, VllmModel):
+    def create(self, model_path, params):
+        VllmModel.create(self, model_path, params)
+
+
+class VllmGemma3w4a16(Gemma3w4a16, VllmModel):
+    def create(self, model_path, params):
+        VllmModel.create(self, model_path, params)
+
+
+class VllmQwen3(Qwen3, VllmModel):
     def create(self, model_path, params):
         VllmModel.create(self, model_path, params)
 
@@ -270,6 +442,13 @@ class Runner:
         "gemma-3-27b-vllm": VllmGemma3(27),
         "gemma-3-12b-vllm": VllmGemma3(12),
         "gemma-3-4b-vllm": VllmGemma3(4),
+        "gemma-3n-4b-vllm": VllmGemma3n(4),
+        "gemma-3-27b-fp8-vllm": VllmGemma3FP8(27),
+        "gemma-3-27b-w4a16-vllm": VllmGemma3w4a16(27),
+        "qwen-3-32b-fp8-vllm": VllmQwen3(32, fp8=True, active=None, instruct=None),
+        "qwen-3-235b-a22b-fp8-vllm": VllmQwen3(235, active=22, fp8=True, instruct=True),
+        "gpt-oss-120b-vllm": VllmGptOss(120),
+        "gpt-oss-20b-vllm": VllmGptOss(20),
         "deepseek-llama-8b": DeepSeek("Llama", 8),
         "deepseek-llama-70b": DeepSeek("Llama", 70),
         "deepseek-qwen-14b": DeepSeek("Qwen", 14),
