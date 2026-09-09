cb77e11cbab98b5ee0966f535ffb4aca784a1d6c
ZJaume <jzaragoza@prompsit.com>
2026-09-09 16:46:38 +0200

    Azerbaijaini default to North Azerbaijani

--- pipeline/langs/maps.py
+++ pipeline/langs/maps.py
@@ -1496,6 +1496,7 @@ BICLEANER_AI_DEFAULTS_MAP = {
 }
 
 ISO6393_DEFAULTS_MAP = {
+    "az": "azj_Latn",
     # ICU returns Kore by default which is a mix of Hang and Hani
     "ko": "kor_Hang",
     # zh is a macro language, map to Mandarin Chinese by default
