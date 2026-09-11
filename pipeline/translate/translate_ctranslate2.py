81aec294f25f3b6464808503ad24e6d59f58ad34
ZJaume <jzaragoza@prompsit.com>
2026-09-11 15:31:34 +0200

    skip setting secrets if not in taskcluster

--- pipeline/translate/translate_ctranslate2.py
+++ pipeline/translate/translate_ctranslate2.py
@@ -218,10 +218,13 @@ class TranslatorIndicTrans2(Translator):
         device_index: list[int],
     ):
         from indictrans2_ct2_inference.translate import Translator as IndicTrans2Inference
-        from pipeline.common.secrets import Secrets
 
-        secrets = Secrets()
-        secrets.prepare_key_hf()
+        # Check if we are inside a taskcluster task
+        import os
+        if os.environ.get("TASK_ID"):
+            from pipeline.common.secrets import Secrets
+            secrets = Secrets()
+            secrets.prepare_key_hf()
 
         self.maxi_batch_size = 10000
         self.model = IndicTrans2Inference(
